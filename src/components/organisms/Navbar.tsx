daff288de8d0c09c40565d39eb405bcc4b0f31a2
Nimesha Edirisinghe <nimesha.edirisinghe@ascentic.se>
2025-09-25 14:46:30 +0530

    chore: added shadow to navbar

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -5,7 +5,7 @@ import { ThemeSwitcher } from "@/components/molecules";
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 w-full border-b bg-background backdrop-blur">
+    <nav className="sticky top-0 z-50 w-full border-b shadow-sm bg-background backdrop-blur">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <div></div>
         <Link href="/" className="flex items-center space-x-2">
